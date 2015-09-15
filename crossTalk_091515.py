2e0d6c3a131d8bb32a5e189e3713e7c99132cd96
Luis Sanchez <lvsanche@ucsd.edu>
2015-09-15 14:20:40 -0700

    Another crosstalk measurement using one coupler and a fiber

---
+++ crossTalk_091515.py
@@ -0,0 +1,52 @@
+#new crosstalk measurement
+#date 15th september 2015
+
+from structureFiling import *
+
+def main():
+	directory = creating_direc('crossTalk_091515') #change this to be the name of the directory, perhaps just the method name
+
+	portName = "port_"
+
+
+	osa = DataCollector_agilent86142B("TCPIP::192.168.1.201::gpib0,27::INSTR", directory)
+	switch = ivi.dicon.diconGP700("TCPIP::192.168.1.201::gpib0,3::INSTR")
+
+	osaFileCrossPeaks = osa.openFile('crosstalkPeaks') #call this so you know what measurement was made with this instrument
+	osaFileCrossPeaks.writerow(['# Outputs: 1 2 3 4'])
+		
+	inputVal = 0
+	inputPortNum = -1
+	while(inputVal != "E"):
+		inputVal = raw_input("Possible commands:(I)nput Configuration, (O)utput sweep, (E)xit: ")
+		
+		
+
+		if inputVal == "I":
+			print("Current inputPort: %i" %inputPortNum)
+			inputPortNum = input ("What is the desired input port? ")
+			portPeaks = [0] *4
+			portnam = portName + str(inputPortNum) +"# "
+		elif inputVal == "O":
+			print "Now sweeping..."
+			for port in range(4):
+				p = port + 1
+				print "Port %d is being checked..." %p
+				
+				switch.switches[1].output = p 
+
+				time.sleep(5) 
+
+				osa.driver._write('calculate1:marker1:maximum')
+				
+				portPeaks[port]= float(osa.driver._ask('calculate1:marker1:Y?'))
+				time.sleep(5)
+
+			osaFileCrossPeaks.writerow([portnam, portPeaks])
+
+		elif inputVal == "E":
+			break
+
+	osa.closeOutputs()
+
+main()
--- structureFiling.py
+++ structureFiling.py
@@ -1,37 +1,106 @@
 import time
 import os
-import shelve
+import ivi
+import csv
 #data file structure for this method
 #will have the following structure:
-''' date_experimentName
-	|
-	trialnumber(maybe time based)
-	|
-	config.txt, some notes on the experiment
-	and metadata
-	|
-	############## this becomes a unique to this situation structure
-	PortNumber
-	|
-	OSA data file
-	Packetloss data file
-	bit error occurance data
-	bit error location data
-	extra trace and noted files 
+''' experiment name
+	date
+	time
+	.csv files for all the fields 
+		- will have meta data 
+		- real data
+		- some note files
 
 
 '''
 	#Method to check and or make new directories for the test that is about to happen
 def creating_direc(methodName):
 	#look for experimentname + date folder 
-	date = time.strftime("%m%d%Y")
-	clock = time.strftime("%H%M%S")
-	dire = "./SummerExperiments/%s_%s/trial_%s/trial%s_shelve" %(date, methodName, clock, clock)
+	date = time.strftime("%Y_%m_%d")
+	clock = time.strftime("%H:%M:%S")
+	dire = "./%s/%s/trial_%s/" %(methodName, date, clock)
 	
    	d = os.path.dirname(dire)
    	if not os.path.exists(d):
    		os.makedirs(d)
 
-   	shelv = shelve.open(dire)
-   	shelv.close()
    	return dire
+
+
+
+
+class DataCollector_agilent8156A:
+	#will produce a file called agilent8156A-META or it will be a the head of the file
+	#agilent8156A-data
+	def __init__(self, address, direc):
+		self.driver = ivi.agilent.agilent8156A(address)
+		self.driver._interface.timeout = 30
+		self.outputFiles = []
+		self.directory = direc
+   
+
+	def openFile(self, measurementToRecord):
+		nameOfFile = self.directory+"agilent8156A_"+measurementToRecord
+		file = open(nameOfFile, 'ab')
+		fileWriter = csv.writer(file, quotechar ='|', quoting=csv.QUOTE_MINIMAL)
+		self.outputFiles.append(file)
+
+		#time to gather metadata and put in the file:
+		fileWriter.writerow(['ID: ', self.driver._load_id_string()])
+		fileWriter.writerow(['Attenuation: ', self.driver._get_attenuation()])
+		fileWriter.writerow(['Offset: ', self.driver._get_offset() ])
+		fileWriter.writerow(['Wavelength: ', self.driver._get_wavelength()])
+		fileWriter.writerow(['Enabled: ', self.driver._get_disable()])
+		fileWriter.writerow(['END OF META DATA'])
+		#debugprint ("ATT+ " + self.directory)
+
+		return fileWriter
+
+
+	def sendDataCommand(self, command, fileOutputWriter):
+		data = self.driver._ask(command)
+		fileOutputWriter.writerow(['%s- Command %s yielded: ' %(time.strftime("%H:%M:%S"),command), data])
+	
+	def closeOutputs(self):
+		for writer in self.outputFiles:
+			writer.close()
+
+class DataCollector_agilent86142B:
+	def __init__(self, address, direc):
+		self.driver = ivi.agilent.agilent86142B(address)
+		self.driver._interface.timeout=30
+		self.outputFiles = []
+		self.directory = direc
+
+	def openFile(self, measurementToRecord):
+		nameOfFile = self.directory+'agilent86142B_'+measurementToRecord
+		file = open(nameOfFile, 'wb')
+		fileWriter = csv.writer(file, quotechar ='|', quoting=csv.QUOTE_MINIMAL)
+		self.outputFiles.append(file)
+
+		#time to gather metadata and put in the file:
+		fileWriter.writerow(['ID: ', self.driver._load_id_string()])
+		fileWriter.writerow(['Acquisition Detector type', self.driver._get_acquisition_detector_type() ])
+		fileWriter.writerow(['Start Wavelength', self.driver._get_wavelength_start() ])
+		fileWriter.writerow(['Stop Wavelength', self.driver._get_wavelength_stop() ])
+		fileWriter.writerow(['Wavelength Offset', self.driver._get_wavelength_offset() ])
+		fileWriter.writerow(['Level Reference', self.driver._get_level_reference() ])
+		fileWriter.writerow(['Level Reference Offset', self.driver._get_level_reference_offset() ])
+		fileWriter.writerow(['Sweep Coupling Resol. Bandwidth', self.driver._get_sweep_coupling_resolution_bandwidth() ])
+		fileWriter.writerow(['Sweeping Continously', self.driver._get_acquisition_sweep_mode_continuous() ])
+		fileWriter.writerow(['Sweep Coupling Sweep time', self.driver._get_sweep_coupling_sweep_time() ])
+		fileWriter.writerow(['Acquisition Vertical Scale', self.driver._get_acquisition_vertical_scale()])
+		fileWriter.writerow(['Sweep Coupling Video Bandwidth', self.driver._get_sweep_coupling_video_bandwidth() ])
+		fileWriter.writerow(['END OF META DATA'])
+
+		#debugprint ("ok: " + self.directory)
+		return fileWriter
+
+	def sendDataCommand(self, command, fileOutputWriter):
+		data = self.driver._ask(command)
+		fileOutputWriter.writerow(['%s- Command %s yielded: ' %(time.strftime("%H:%M:%S"),command), data])
+
+	def closeOutputs(self):
+		for writer in self.outputFiles:
+			writer.close()
